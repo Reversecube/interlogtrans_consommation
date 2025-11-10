9d998b73f12f5ba40b842d0f1cad89dbd9c36f50
lziyat <anass.mrme@gmail.com>
2025-11-10 10:25:53 +0100

    set new update

--- __manifest__.py
+++ __manifest__.py
@@ -1,16 +1,17 @@
 {
     'name': 'Interlog Trans - Gestion de Consommation',
-    'version': '1.0.2',
-    'category': 'Fleet',
     'summary': 'Gestion de la consommation de carburant',
     'description': 'Développé par Reversecube',
     'author': 'Reversecube',
+    'version': '17.0.1.0.2',  # Increment version
     'website': 'https://github.com/Reversecube',
+    'category': 'Fleet',
     'depends': ['fleet'],
     'data': [
-        # 'security/ir.model.access.csv',
+        'security/ir.model.access.csv',
         'views/interlogtrans_consommation_views.xml',
     ],
     'installable': True,
     'application': True,
+    'license': 'LGPL-3',
 }
--- models/interlogtrans_consommation.py
+++ models/interlogtrans_consommation.py
@@ -6,22 +6,28 @@ class InterlogtransConsommation(models.Model):
     _inherit = 'fleet.vehicle.cost'
     _order = 'date desc'
     
-    vehicle_id = fields.Many2one('fleet.vehicle', 'Véhicule', required=True)
-    kilometrage = fields.Float('Kilométrage', digits=(16, 2))
-    kilometrage_precedent = fields.Float('Kilométrage Précédent', digits=(16, 2))
-    distance_parcourue = fields.Float('Distance', compute='_compute_distance', store=True)
-    compteur = fields.Char('Station')
-    article = fields.Selection([('gasoil', 'GASOIL'), ('adblue', 'ADBLUE')], 'Carburant', default='gasoil')
-    quantite = fields.Float('Quantité', digits=(16, 2))
-    prix_par_litre = fields.Float('Prix/L', digits=(16, 2))
-    consommation = fields.Float('L/100km', compute='_compute_consommation', store=True)
+    vehicle_id = fields.Many2one('fleet.vehicle', string='Véhicule', required=True)
+    kilometrage = fields.Float(string='Kilométrage', digits=(16, 2))
+    kilometrage_precedent = fields.Float(string='Kilométrage Précédent', digits=(16, 2))
+    distance_parcourue = fields.Float(string='Distance', compute='_compute_distance', store=True)
+    compteur = fields.Char(string='Station')
+    article = fields.Selection([('gasoil', 'GASOIL'), ('adblue', 'ADBLUE')], string='Carburant', default='gasoil')
+    quantite = fields.Float(string='Quantité', digits=(16, 2))
+    prix_par_litre = fields.Float(string='Prix/L', digits=(16, 2))
+    consommation = fields.Float(string='L/100km', compute='_compute_consommation', store=True)
     
     @api.depends('kilometrage', 'kilometrage_precedent')
     def _compute_distance(self):
         for rec in self:
-            rec.distance_parcourue = abs(rec.kilometrage - rec.kilometrage_precedent) if rec.kilometrage and rec.kilometrage_precedent else 0.0
+            if rec.kilometrage and rec.kilometrage_precedent:
+                rec.distance_parcourue = abs(rec.kilometrage - rec.kilometrage_precedent)
+            else:
+                rec.distance_parcourue = 0.0
     
     @api.depends('quantite', 'distance_parcourue')
     def _compute_consommation(self):
         for rec in self:
-            rec.consommation = (rec.quantite / rec.distance_parcourue) * 100 if rec.distance_parcourue > 0 and rec.quantite > 0 else 0.0
+            if rec.distance_parcourue > 0 and rec.quantite > 0:
+                rec.consommation = (rec.quantite / rec.distance_parcourue) * 100.0
+            else:
+                rec.consommation = 0.0
