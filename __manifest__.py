56409938d0fc251e65df180c18ae3fe0bef8d9c1
lziyat <anass.mrme@gmail.com>
2025-11-10 11:49:26 +0100

    set new test update

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Interlog Trans - Gestion de Consommation',
-    'version': '17.0.1.1.0',  # Change version to trigger upgrade
+    'version': '17.0.1.0.1',
     'category': 'Fleet',
     'summary': 'Module de gestion de consommation de carburant',
     'author': 'Développé par Interlog Trans',
